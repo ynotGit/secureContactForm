0f7b1df408f6567e0f949fc72d6484fe8fb78ed3
Tony Porter <tonyporter89@gmail.com>
2019-01-07 15:36:47 +0000

    bug fix

--- form.php
+++ form.php
@@ -122,7 +122,7 @@
       </form>
     </div>
   </section>
-
+  <script defer src='https://www.google.com/recaptcha/api.js'></script>
   <!-- Materialize jS-->
   <script defer src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
   <script defer src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
